3f28bd74cacd157a7f01a02d89c0bd7b7ed4a55b
U202210138 <129425974+U202210138@users.noreply.github.com>
2024-04-23 12:32:25 -0500

    feat(home): added toolbar content in home components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarContentComponent } from './public/components/home/components/toolbar-content/toolbar-content.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatButtonModule} from '@angular/material/button';
+import { HomeContentComponent } from './public/components/home/components/home-content/home-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarContentComponent,
+    HomeContentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbar,
+    MatButtonModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/public/components/home/components/toolbar-content/toolbar-content.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toolbar-content',
+  templateUrl: './toolbar-content.component.html',
+  styleUrl: './toolbar-content.component.css'
+})
+export class ToolbarContentComponent {
+
+}
