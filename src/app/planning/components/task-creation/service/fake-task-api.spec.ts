e018a6ca504f85e2c7fd76eba2f04be0d650c479
SEBRH <u202211894@upc.edu.pe>
2024-04-24 19:46:39 -0500

    fix(planning): deleted unused files

--- src/app/planning/components/task-creation/service/fake-task-api.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FakeApiService } from './fake-api-task';
-
-describe('FakeApiService', () => {
-  it('should create an instance', () => {
-    // @ts-ignore
-    expect(new FakeApiService()).toBeTruthy();
-  });
-});
