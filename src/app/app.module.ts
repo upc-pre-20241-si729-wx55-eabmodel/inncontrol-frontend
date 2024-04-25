a940788b72eb73ba9e239fb8dd0cbfc43b276008
Ryzeon <alex.ramon2004@gmail.com>
2024-04-25 03:57:29 -0500

    chore: remove unused import.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ToolbarContentComponent } from './public/components/home/components/toolbar-content/toolbar-content.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ToolbarContentComponent} from './public/components/home/components/toolbar-content/toolbar-content.component';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {MatToolbar} from "@angular/material/toolbar";
 import {MatButtonModule} from '@angular/material/button';
-import { HomeContentComponent } from './public/components/home/components/home-content/home-content.component';
-import { ReportFormComponent } from './interactions/report/components/report-form/report-form.component';
+import {HomeContentComponent} from './public/components/home/components/home-content/home-content.component';
+import {ReportFormComponent} from './interactions/report/components/report-form/report-form.component';
 import {MatCardModule} from "@angular/material/card";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatOption, MatSelect} from "@angular/material/select";
@@ -39,4 +39,5 @@ import {MatInput} from "@angular/material/input";
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
