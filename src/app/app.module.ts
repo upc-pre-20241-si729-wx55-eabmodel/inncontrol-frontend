15859aebe7898f50ebd6e63afdc5de934e0e7cc2
U202210138 <129425974+U202210138@users.noreply.github.com>
2024-04-25 13:12:11 -0500

    feat(inventory): imported angular materials dialog functions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,23 +8,61 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import {MatToolbar} from "@angular/material/toolbar";
 import {MatButtonModule} from '@angular/material/button';
 import { HomeContentComponent } from './public/components/home/components/home-content/home-content.component';
-import { InventoryContentComponent } from './inventory/components/inventory-content/inventory-content.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatDatepickerInput} from "@angular/material/datepicker";
+import {MatDatepickerToggle} from "@angular/material/datepicker";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatNativeDateModule} from "@angular/material/core";
+import {provideNativeDateAdapter} from "@angular/material/core";
+import {DialogOverviewItemDialog} from "./inventory/components/inventory-add-item-button/inventory-add-item-button.component";
+import {addItemButton} from "./inventory/components/inventory-add-item-button/inventory-add-item-button.component";
+import {
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogModule,
+  MatDialogTitle
+} from "@angular/material/dialog";
+import {FormsModule} from "@angular/forms";
+import {InventoryContentComponent} from "./inventory/components/inventory-content/inventory-content.component";
+
+// @ts-ignore
 
 @NgModule({
   declarations: [
     AppComponent,
     ToolbarContentComponent,
     HomeContentComponent,
-    InventoryContentComponent
+    InventoryContentComponent,
+    DialogOverviewItemDialog,
+    addItemButton
+
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatToolbar,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerInput,
+    MatDatepickerToggle,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatDialogActions,
+    MatDialogContent,
+    MatDialogClose,
+    MatDialogTitle,
+    MatDialogModule,
+    FormsModule,
+
   ],
   providers: [
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+    provideNativeDateAdapter()
+
   ],
   bootstrap: [AppComponent]
 })
