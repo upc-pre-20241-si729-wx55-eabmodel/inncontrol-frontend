f16ddfe2fe26d79fdd129e2e17e3a42972192a21
U202210138 <129425974+U202210138@users.noreply.github.com>
2024-04-25 13:11:24 -0500

    feat(inventory): added item entity

---
+++ src/app/inventory/model/item.entity.ts
@@ -0,0 +1,14 @@
+export class ItemEntity {
+
+  id: string;
+  name: string;
+  description: string;
+  quantity: number;
+
+  constructor(id: string, name: string, description: string, quantity: number) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.quantity = quantity;
+  }
+}
