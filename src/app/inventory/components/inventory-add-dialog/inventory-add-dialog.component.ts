f6500dace75951b7325c54810f8249f8a6274a4d
U202210138 <129425974+U202210138@users.noreply.github.com>
2024-04-26 18:06:21 -0500

    feat(add-item): added item validators

--- src/app/inventory/components/inventory-add-dialog/inventory-add-dialog.component.ts
+++ src/app/inventory/components/inventory-add-dialog/inventory-add-dialog.component.ts
@@ -1,6 +1,7 @@
 import {Component, Inject} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import {Item} from "../../model/item.entity";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-inventory-add-dialog',
@@ -8,13 +9,36 @@ import {Item} from "../../model/item.entity";
   styleUrl: './inventory-add-dialog.component.css'
 })
 export class InventoryAddDialogComponent {
-  constructor(
+  reportItemFormGroup: FormGroup;
+  constructor(private formBuilder: FormBuilder,
     public dialogRef: MatDialogRef<InventoryAddDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: Item,
   ) {
+    this.reportItemFormGroup = this.formBuilder.group({
+      name: new FormControl('', [
+        Validators.required,
+        Validators.minLength(2)  // This line ensures a minimum of 2 characters
+      ]),
+      description: new FormControl('', [
+        Validators.required,
+        Validators.minLength(2)  // This line ensures a minimum of 2 characters
+      ]),
+      quantity: new FormControl(0, [
+        Validators.required,
+        Validators.min(0),
+        Validators.pattern("^[0-9]*$")  // This line ensures only numeric input
+      ])
+    });
   }
 
   onNoClick(): void {
     this.dialogRef.close();
+  }
+
+  onSubmit(): void {
+    if (this.reportItemFormGroup.valid) {
+      this.data = this.reportItemFormGroup.value;
+      this.dialogRef.close(this.data);
+    }
   }
 }
