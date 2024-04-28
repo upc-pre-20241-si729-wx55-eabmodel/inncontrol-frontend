11ce480caadf9105fca5c581bfd2183349bc4359
U202210138 <129425974+U202210138@users.noreply.github.com>
2024-04-27 22:31:07 -0500

    feat(add-item-table): created example of angular material table

---
+++ src/app/inventory/components/inventory-table/inventory-table.component.ts
@@ -0,0 +1,76 @@
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {MatSort, MatSortModule} from '@angular/material/sort';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {InventoryAddDialogComponent} from "../inventory-add-dialog/inventory-add-dialog.component";
+
+export interface UserData {
+  id: string;
+  name: string;
+  progress: string;
+  fruit: string;
+}
+
+const FRUITS: string[] = [
+  'blueberry',
+  'lychee',
+
+];
+const NAMES: string[] = [
+  'Maia',
+  'Asher',
+  'Olivia'
+];
+
+@Component({
+  selector: 'app-inventory-table',
+  styleUrl: 'inventory-table.component.css',
+  templateUrl: 'inventory-table.component.html',
+})
+export class InventoryTableComponent implements AfterViewInit {
+  displayedColumns: string[] = ['id', 'item', 'description', 'quantity'];
+  dataSource: MatTableDataSource<UserData>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor() {
+    // Create 100 users
+    const users = Array.from({length: 100}, (_, k) => createNewUser(k + 1));
+
+    // Assign the data to the data source for the table to render
+    this.dataSource = new MatTableDataSource(users);
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+}
+
+/** Builds and returns a new User. */
+function createNewUser(id: number): UserData {
+  const name =
+    NAMES[Math.round(Math.random() * (NAMES.length - 1))] +
+    ' ' +
+    NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) +
+    '.';
+
+  return {
+    id: id.toString(),
+    name: name,
+    progress: Math.round(Math.random() * 100).toString(),
+    fruit: FRUITS[Math.round(Math.random() * (FRUITS.length - 1))],
+  };
+}
