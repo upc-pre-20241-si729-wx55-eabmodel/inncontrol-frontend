dd3336d0475cdfdfce0b5f583267f3fc4cedde39
U202210138 <129425974+U202210138@users.noreply.github.com>
2024-04-28 00:45:56 -0500

    feat(inventory-add-item-button): Even emitter added

--- src/app/inventory/components/inventory-add-item-button/inventory-add-item-button.component.ts
+++ src/app/inventory/components/inventory-add-item-button/inventory-add-item-button.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 import {Item} from "../../model/item.entity";
 import {MatDialog} from "@angular/material/dialog";
 import {InventoryAddDialogComponent} from "../inventory-add-dialog/inventory-add-dialog.component";
@@ -9,8 +9,8 @@ import {InventoryAddDialogComponent} from "../inventory-add-dialog/inventory-add
     styleUrl: './inventory-add-item-button.component.css'
 })
 export class InventoryAddItemButtonComponent {
-    items: Item[] = [];
-    selectedItem: Item | null = null;
+  @Output() itemCreated = new EventEmitter<Item>();
+  selectedItem: Item | null = null;
 
     constructor(private dialog: MatDialog) {
     }
@@ -22,9 +22,9 @@ export class InventoryAddItemButtonComponent {
 
         dialogRef.afterClosed().subscribe(result => {
             if (result) {
-                this.items.push(result);
-                console.log(this.items);
                 this.selectedItem = result;
+                // @ts-ignore
+              this.itemCreated.emit(this.selectedItem);
             }
         });
     }
