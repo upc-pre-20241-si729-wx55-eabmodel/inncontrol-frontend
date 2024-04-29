044e94d7d9681d809f9e7593068e16b587f8d270
Ryzeon <alex.ramon2004@gmail.com>
2024-04-29 17:15:45 -0500

    refactor(task): relocated.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ToolbarContentComponent } from './public/components/home/components/toolbar-content/toolbar-content.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ToolbarContentComponent} from './public/components/home/components/toolbar-content/toolbar-content.component';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {MatToolbar} from "@angular/material/toolbar";
 import {MatButtonModule} from '@angular/material/button';
-import { HomeContentComponent } from './public/components/home/components/home-content/home-content.component';
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
 import {MatDatepickerInput} from "@angular/material/datepicker";
@@ -14,7 +13,6 @@ import {MatDatepickerToggle} from "@angular/material/datepicker";
 import {MatDatepickerModule} from "@angular/material/datepicker";
 import {MatNativeDateModule, MatOption} from "@angular/material/core";
 import {provideNativeDateAdapter} from "@angular/material/core";
-import {TaskCreator} from "./planning/components/task-creation/components/task-creation.component";
 import {
   MatDialogActions,
   MatDialogClose,
@@ -23,8 +21,6 @@ import {
   MatDialogTitle
 } from "@angular/material/dialog";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import { PerformanceReport } from './planning/page/performance-report/performance-report';
-import { TaskCreateDialogComponent } from './display/components/task-create-dialog/task-create-dialog/task-create-dialog.component';
 import {
   MatCell,
   MatCellDef,
@@ -40,18 +36,12 @@ import {MatIcon} from "@angular/material/icon";
 import {MatSelect} from "@angular/material/select";
 
 
-
-
 // @ts-ignore
 
 @NgModule({
   declarations: [
     AppComponent,
     ToolbarContentComponent,
-    HomeContentComponent,
-    TaskCreator,
-    PerformanceReport,
-    TaskCreateDialogComponent
   ],
   imports: [
     BrowserModule,
@@ -96,11 +86,7 @@ import {MatSelect} from "@angular/material/select";
 
   ],
   bootstrap: [AppComponent]
-
-
 })
 
-// timepicker uses these component
-
-
-export class AppModule { }
+export class AppModule {
+}
