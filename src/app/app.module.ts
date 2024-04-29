4f8f37e9a112c49bc5f75c52811e774c021cd9ec
Alex Avila <alex.ramon2004@gmail.com>
2024-04-28 21:48:23 -0500

    refactor(app): organize imports.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,65 +1,54 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ToolbarContentComponent } from './public/components/home/components/toolbar-content/toolbar-content.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import {MatToolbar} from "@angular/material/toolbar";
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ToolbarContentComponent} from './public/components/home/components/toolbar-content/toolbar-content.component';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatButtonModule} from '@angular/material/button';
-import { HomeContentComponent } from './public/components/home/components/home-content/home-content.component';
 import {MatFormFieldModule} from "@angular/material/form-field";
-import {MatInputModule} from "@angular/material/input";
-import {MatDatepickerInput} from "@angular/material/datepicker";
-import {MatDatepickerToggle} from "@angular/material/datepicker";
-import {MatDatepickerModule} from "@angular/material/datepicker";
-import {MatNativeDateModule, MatOption} from "@angular/material/core";
-import {provideNativeDateAdapter} from "@angular/material/core";
+import {MatOptionModule, provideNativeDateAdapter} from "@angular/material/core";
 import {TaskCreator} from "./planning/components/task-creation/components/task-creation.component";
-import {
-  MatDialogActions,
-  MatDialogClose,
-  MatDialogContent,
-  MatDialogModule,
-  MatDialogTitle
-} from "@angular/material/dialog";
+import {MatDialogModule} from "@angular/material/dialog";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import { TaskViewComponent } from './planning/page/task-view/task-view.component';
-import { TaskCreateDialogComponent } from './display/components/task-create-dialog/task-create-dialog/task-create-dialog.component';
+import {TaskViewComponent} from './planning/page/task-view/task-view.component';
 import {
-  MatCell,
-  MatCellDef,
-  MatColumnDef,
-  MatHeaderCell, MatHeaderCellDef,
-  MatHeaderRow, MatHeaderRowDef, MatNoDataRow,
-  MatRow, MatRowDef,
-  MatTable
-} from "@angular/material/table";
-import {MatPaginator} from "@angular/material/paginator";
-import {MatSort, MatSortHeader} from "@angular/material/sort";
-import {MatIcon} from "@angular/material/icon";
-import {MatSelect} from "@angular/material/select";
+  TaskCreateDialogComponent
+} from './display/components/task-create-dialog/task-create-dialog/task-create-dialog.component';
+import {MatSelectModule} from "@angular/material/select";
 import {
   NotificationsCardComponent
 } from './monitoring/notifications/components/notifications-card/notifications-card.component';
-import {MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle} from "@angular/material/dialog";
 import {
   NotificationsViewComponent
 } from './monitoring/notifications/components/notifications-view/notifications-view.component';
 import {
   NotificationsBadgeComponent
 } from './monitoring/notifications/components/notifications-badge/notifications-badge.component';
-import {MatBadge} from "@angular/material/badge";
-import {MatProgressBar} from "@angular/material/progress-bar";
-import {MatDivider} from "@angular/material/divider";
+import {MatBadgeModule} from "@angular/material/badge";
+import {MatProgressBarModule} from "@angular/material/progress-bar";
+import {MatDividerModule} from "@angular/material/divider";
+import {MatCardModule} from "@angular/material/card";
+import {MatInputModule} from "@angular/material/input";
+import {MatIconModule} from "@angular/material/icon";
+import {HttpClientModule} from "@angular/common/http";
+import {SearchContentComponent} from "./public/components/home/components/search-content/search-content.component";
+import {EmployeesComponent} from "./public/pages/employees/employees.component";
+import {MessagesComponent} from "./public/pages/messages/messages.component";
+import {RoomStateComponent} from "./public/pages/room-state/room-state.component";
+import {TasksComponent} from "./public/pages/tasks/tasks.component";
+import {PageNotFoundComponent} from "./public/pages/page-not-found/page-not-found.component";
+import {HomeComponent} from "./public/pages/home/home.component";
+import {InventoryContentComponent} from "./inventory/components/inventory-content/inventory-content.component";
+import {ReportFormComponent} from "./interactions/report/components/report-form/report-form.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     ToolbarContentComponent,
-    HomeContentComponent,
     TaskCreator,
     TaskViewComponent,
-    TaskCreateDialogComponent
+    TaskCreateDialogComponent,
     ReportFormComponent,
     InventoryContentComponent,
     HomeComponent,
@@ -77,23 +66,20 @@ import {MatDivider} from "@angular/material/divider";
     BrowserModule,
     AppRoutingModule,
     MatCardModule,
-    MatToolbar,
+    MatToolbarModule,
     MatButtonModule,
     MatFormFieldModule,
-    MatSelect,
-    MatOption,
+    MatSelectModule,
+    MatOptionModule,
     ReactiveFormsModule,
-    MatInput,
+    MatInputModule,
     MatIconModule,
     HttpClientModule,
     FormsModule,
-    MatDialogActions,
-    MatDialogClose,
-    MatDialogContent,
-    MatDialogTitle,
-    MatBadge,
-    MatProgressBar,
-    MatDivider,
+    MatDialogModule,
+    MatBadgeModule,
+    MatProgressBarModule,
+    MatDividerModule,
   ],
   providers: [
     provideAnimationsAsync(),
