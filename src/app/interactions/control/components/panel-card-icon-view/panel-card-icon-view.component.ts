b03d12ce654aafc8424d4f20f2c871f6aabcbf72
Alex Avila <alex.ramon2004@gmail.com>
2024-05-19 15:11:46 -0500

    feat: init panel card view component.

---
+++ src/app/interactions/control/components/panel-card-icon-view/panel-card-icon-view.component.ts
@@ -0,0 +1,13 @@
+import {Component, Input} from '@angular/core';
+import {PanelCard} from "../../model/panel-card";
+
+@Component({
+  selector: 'app-panel-card-icon-view',
+  templateUrl: './panel-card-icon-view.component.html',
+  styleUrl: './panel-card-icon-view.component.css'
+})
+export class PanelCardIconViewComponent {
+
+  @Input() title: string;
+  @Input() icons: PanelCard[]
+}
