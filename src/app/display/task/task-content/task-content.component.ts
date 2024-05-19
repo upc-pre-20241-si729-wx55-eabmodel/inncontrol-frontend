bf67a1af59bdd3cbc2b32fd994adb77d8675fe47
U202210138 <129425974+U202210138@users.noreply.github.com>
2024-05-19 12:22:00 -0500

    feat(task-content-create): changed task content

--- src/app/display/task/task-content/task-content.component.ts
+++ src/app/display/task/task-content/task-content.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {MatTableDataSource} from "@angular/material/table";
 import {MatSort} from "@angular/material/sort";
 import {MatPaginator} from "@angular/material/paginator";
@@ -10,26 +10,46 @@ import {TaskApiService} from "../task-create/services/task-api.service";
   templateUrl: './task-content.component.html',
   styleUrl: './task-content.component.css'
 })
-export class TaskContentComponent implements AfterViewInit {
-  tasks: Task[] = [];
-
+export class TaskContentComponent implements OnInit,AfterViewInit {
+  tasksData: Task;
   length: number = 1;
-
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
   displayedColumns: string[] = ['id', 'taskName', 'description', 'dueDate', 'status', 'userid', 'creationDate'];
   dataSource: MatTableDataSource<Task>;
 
   constructor(private taskService: TaskApiService) {
-    this.dataSource = new MatTableDataSource(this.tasks);
+    this.dataSource = new MatTableDataSource<any>();
+    this.tasksData = {} as Task;
   }
+
+
+
+  private getAllTasks(): void {
+    this.taskService.getAll().subscribe((response: any)=>{
+      this.dataSource.data = response;
+    })
+  }
+
+  private createTask(){
+    this.taskService.create(this.tasksData).subscribe((response: any)=>{
+      this.dataSource.data.push({...response});
+      this.dataSource.data = this.dataSource.data.map((task: Task) =>{
+        return task;
+      });
+    });
+  };
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
-    this.getTasks();
   }
 
+  ngOnInit() {
+   this.getAllTasks();
+  }
+
+  /*
   getTasks(): void {
     this.taskService.getTasks().subscribe(tasks => {
       this.tasks = tasks;
@@ -47,7 +67,7 @@ export class TaskContentComponent implements AfterViewInit {
       this.tasks.push(task);
       this.dataSource._updateChangeSubscription();
     });
-  }
+  }*/
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
