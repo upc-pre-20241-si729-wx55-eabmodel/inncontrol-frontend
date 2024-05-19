fdc50ac406ef9e169f956feddcf53b2c5a0400df
Alex Avila <alex.ramon2004@gmail.com>
2024-05-19 15:11:54 -0500

    feat: init panel card content component.

---
+++ src/app/interactions/control/components/panel-content/panel-content.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-panel-content',
+  templateUrl: './panel-content.component.html',
+  styleUrl: './panel-content.component.css'
+})
+export class PanelContentComponent {
+
+}
