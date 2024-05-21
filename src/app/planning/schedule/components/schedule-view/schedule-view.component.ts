422992df29431314c6f7ec4ba700d8c37aeea9f1
Alex Avila <alex.ramon2004@gmail.com>
2024-05-21 16:45:44 -0500

    feat: init schedule view component.

---
+++ src/app/planning/schedule/components/schedule-view/schedule-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-schedule-view',
+  templateUrl: './schedule-view.component.html',
+  styleUrl: './schedule-view.component.css'
+})
+export class ScheduleViewComponent {
+
+}
