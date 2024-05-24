803519d5c879c4de3dfb1d768e466d84a85ef8f8
Erick Cueva <u201910151@upc.edu.pe>
2024-05-24 06:50:51 -0500

    feat(inventory): added inventory card container

---
+++ src/app/supply/inventory/inventory-content/inventory-content.component.ts
@@ -0,0 +1,85 @@
+import { Component, OnInit } from '@angular/core';
+import {Inventory} from "../inventory-create/model/inventory.entity";
+import {InventoryApiService} from "../inventory-create/services/inventory-api.service";
+import {MatDialog} from "@angular/material/dialog";
+import {InventoryEditDialogComponent} from "../inventory-create/components/inventory-edit-dialog/inventory-edit-dialog.component";
+import {InventoryCardDialogComponent} from "../inventory-card-dialog/inventory-card-dialog.component";
+import {MatFormField} from "@angular/material/form-field";
+@Component({
+  selector: 'app-inventory-content',
+  templateUrl: './inventory-content.component.html',
+  styleUrl: './inventory-content.component.css'
+})
+export class InventoryContentComponent implements OnInit{
+  inventoryData: Inventory[]=[];
+  item: Inventory;
+  constructor(private inventoryService: InventoryApiService, private dialog: MatDialog) {
+    this.item = new Inventory(0,'','',0,'','','','');
+  }
+
+  private getAllItems():void{
+    this.inventoryService.getAll().subscribe((response:any)=>{
+      this.inventoryData = response;
+    })
+  }
+
+  protected createItem(inventory: Inventory){
+    this.inventoryService.create(inventory).subscribe((response:any) =>{
+      this.inventoryData.push(response);
+    });
+  };
+
+
+
+  handleUpdate(inventory: Inventory):void{
+    this.openUpdateDialog(inventory);
+  }
+  onDeleteItem(element: Inventory) {
+    this.deleteItem(element.id);
+  }
+
+  private deleteItem(inventoryId: number) {
+    this.inventoryService.delete(inventoryId).subscribe(() =>{
+      this.inventoryData = this.inventoryData.filter((inventory: Inventory) => inventory.id !== inventoryId);
+    });
+  }
+  openUpdateDialog(inventory: Inventory){
+    console.log('Update dialog opened');
+    const dialogRef = this.dialog.open(InventoryEditDialogComponent, {
+      data:inventory
+    });
+
+    dialogRef.afterClosed().subscribe(result=>{
+      if(result){
+        this.updateItem(result);
+      }
+    });
+  }
+
+  private updateItem(inventory: Inventory){
+    this.inventoryService.update(inventory.id, inventory).subscribe((response:Inventory)=>{
+      const index = this.inventoryData.findIndex(i=>i.id==inventory.id);
+      if(index!==-1){
+        this.inventoryData[index] = response;
+      }
+    });
+  }
+  openDialog(inventory: any){
+    let item = this.item.getItemById(inventory);
+    console.log('Open Dialog', inventory);
+    const dialogRef = this.dialog.open(InventoryCardDialogComponent, {
+      data:inventory
+    });
+    dialogRef.afterClosed().subscribe(result=>{
+      console.log('The dialog was closed', result);
+      if(result == 'Delete'){
+        this.deleteItem(inventory);
+      }
+    })
+  }
+  ngOnInit() {
+    this.getAllItems();
+  }
+
+  protected readonly Inventory = Inventory;
+}
