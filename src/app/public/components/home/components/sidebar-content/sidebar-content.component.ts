b5bca628b3ce4916ea429b985a510c789eb8e805
Ryzeon <alex.ramon2004@gmail.com>
2024-05-30 23:11:30 -0500

    feat: added sidebar navigation.

---
+++ src/app/public/components/home/components/sidebar-content/sidebar-content.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-content',
+  templateUrl: './sidebar-content.component.html',
+  styleUrl: './sidebar-content.component.css'
+})
+export class SidebarContentComponent {
+
+}
