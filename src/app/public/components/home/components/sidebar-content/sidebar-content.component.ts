5b0a27021879ddb794e0ab10dc64867b8b0a3ce9
U202210138 <129425974+U202210138@users.noreply.github.com>
2024-06-06 15:28:45 -0500

    feat(sidebar-mobile): added function to only apear when its for mobile

--- src/app/public/components/home/components/sidebar-content/sidebar-content.component.ts
+++ src/app/public/components/home/components/sidebar-content/sidebar-content.component.ts
@@ -1,6 +1,9 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Router, NavigationEnd } from '@angular/router';
 import {Component} from '@angular/core';
 import {PanelCard} from "../../../../../shared/model/panel-card";
 import {PanelCardIcon} from "../../../../../shared/model/panel-card-icon";
+import {map, Observable} from "rxjs";
 
 @Component({
   selector: 'app-sidebar-content',
@@ -23,4 +26,10 @@ export class SidebarContentComponent {
   }
 
   protected readonly PanelCardIcon = PanelCardIcon;
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches)
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) { }
 }
