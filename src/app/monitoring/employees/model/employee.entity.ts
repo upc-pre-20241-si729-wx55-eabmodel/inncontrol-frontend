910864035d58b2dab9f63f9627e8c9eeeadace0f
SEBRH <u202211894@upc.edu.pe>
2024-06-06 20:11:12 -0500

    feat(employees): implemented employee DIALOG.

--- src/app/monitoring/employees/model/employee.entity.ts
+++ src/app/monitoring/employees/model/employee.entity.ts
@@ -5,13 +5,15 @@ export class EmployeeEntity {
   serviceHours: string;
   tasks: number;
   role: string;
+  profileImage: string;
 
-  constructor(profileId: number, personName: string, serviceHours: string, tasks: number, role: string){
+  constructor(profileId: number, personName: string, serviceHours: string, tasks: number, role: string, image: string) {
     this.profileId = profileId;
     this.personName = personName;
     this.serviceHours = serviceHours;
     this.tasks = tasks;
     this.role = role;
+    this.profileImage = image;
   }
 
   getRole() {
