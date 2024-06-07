3344c332ec17559d781619083a93999122470d6b
Ryzeon <alex.ramon2004@gmail.com>
2024-06-07 02:43:34 -0500

    feat(footer): added footer.

--- src/app/public/components/home/components/footer-content/footer-content.component.spec.ts
+++ src/app/public/components/home/components/footer-content/footer-content.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomeComponent } from './home.component';
+import { FooterContentComponent } from './footer-content.component';
 
-describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+describe('FooterContentComponent', () => {
+  let component: FooterContentComponent;
+  let fixture: ComponentFixture<FooterContentComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [HomeComponent]
+      declarations: [FooterContentComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(HomeComponent);
+    fixture = TestBed.createComponent(FooterContentComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
