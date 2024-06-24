05833ab092008badc733d88ab946b37d625a6523
Alex Avila <alex.ramon2004@gmail.com>
2024-06-24 18:23:48 -0500

    feat(prod): send to prod.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   serverBasePath: 'http://localhost:8080/api/v1',
   prodBasePath: 'https://inncontrol-api.ryzeon.me/api/v1'
 };
